67b09be63665b210450e758ed49b64066ec3e9a9
Snezhana Pashovska <snezhana.pashovska@hotmail.com>
2022-03-22 11:31:57 +0100

    Modify quantity + delete button + total price

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,128 +1,150 @@
-const products = fetch ("http://localhost:3000/api/products")
-.then(function(res){
-if(res.ok){
-    return res.json();
-}
-})
-.then(products => {
-console.log('Success:', products); 
-})
+// Initialization of local storage//
 
-const productsInLocalStorage = JSON.parse(localStorage.getItem("item"));
+let productsInLocalStorage = JSON.parse(localStorage.getItem("item"));
 console.table(productsInLocalStorage);
-//const productsInCart = document.querySelector(".cartAndFormContainer")
+
+//Variables to calculate the total quantity of products in the cart//
+
+let calculateTotalQuantity =[];
+let totalQuantity = document.getElementById("totalQuantity");
+
+
+//variables to calculate the total price of products in the cart//
+
+let calculateTotalPrice = [];
+let totalPrice =document.getElementById("totalPrice");
+
 const emptyCart = document.querySelector("#cart__items");
-const selectedQuantity = document.querySelector("#quantity");
-//if the cart is empty
+
+
+//The accumulator is the net result of the function. It contains either the initial value or the return value of the last call. currentValue: the value of the current element. 
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+//----------------------------------------Items in cart---------------------------------------------------------//
+
+
+//If the cart is empty
 if(productsInLocalStorage === null){
   const noItems = `<p>Votre panier est vide</p>`;
   emptyCart.innerHTML = noItems;
   console.log (noItems);
 }
-//-----------------------------Retrieving the price from API-----------------------------------------------//
-let items = JSON.parse(localStorage.getItem("item"));
-
-for (let i = 0; i < items.length; i++) {
-  async function fetchJsonProduct() {
-    const response = await fetch(
-      `http://localhost:3000/api/products/${items[i].id}`
-    );
-    return await response.json();
-  }
-  
-//-------------------------Items in cart-------------------------//
-//if there are items in the cart
-function cartItems(){
-   if(productsInLocalStorage !== 0) {
-  
-    //console.log(productsInLocalStorage);
-    fetchJsonProduct()
-
-    .then((info) => {
+//Retrieving the price from API//
+ else { 
       //console.log(info); 
-    const itemsInCart = document.querySelector("#cart__items");
-    
-    for(let item of productsInLocalStorage) {
-   
- ///--------------------------------DOM elements-----------------------------------------------------------------//
-      itemsInCart.insertAdjacentHTML("afterend", `
-              <article class="cart__item" data-id="${info._id}" data-color="${item.color}">
+    productsInLocalStorage.forEach((element) => {
+      fetch( `http://localhost:3000/api/products/${element.id}`)
+            .then((elements) =>  elements.json())
+
+            .then((elements) => {
+  
+//If there are items in the cart
+
+//--------------------------------DOM elements-----------------------------------------------------------------//
+      const itemsInCart = `
+              <article class="cart__item" data-id="${elements._id}" data-color="${element.color}">
                       <div class="cart__item__img">
-                        <img src="${item.image}" alt="${item.altTxt}">
+                        <img src="${elements.imageUrl}" alt="${elements.altTxt}">
                       </div>
                       <div class="cart__item__content">
                         <div class="cart__item__content__description">
-                          <h2>${info.name}</h2>
-                          <p>${item.color}</p>
-                          <p>${info.price}€</p>
+                          <h2>${elements.name}</h2>
+                          <p>${element.color}</p>
+                          <p>${elements.price}€</p>
                         </div>
                         <div class="cart__item__content__settings">
                           <div class="cart__item__content__settings__quantity">
                             <p>Qté : </p>
-                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}">
+                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${element.quantity}">
                           </div>
                           <div class="cart__item__content__settings__delete">
                             <p class="deleteItem">Supprimer</p>
                           </div>
                         </div>
                       </div>
-                    </article>`);
+                    </article>`;
 
+                    document
+                      .getElementById("cart__items")
+                      .insertAdjacentHTML("afterbegin", itemsInCart);
+                      deleteProduct()
+                      ModifyQuantity()
 
+//------------------------Total quantity---------------//
 
-let totalQuantity = document.getElementById("totalQuantity");
-let totalPrice = document.getElementById("totalPrice");
-let productId = document.getElementsByClassName("data-id");
-let productColor = document.getElementsByClassName("data-color");
-let productQuantity = item.quantity;
-let productName = item.name;
-console.log(productQuantity);
+calculateTotalQuantity.push(parseInt(`${element.quantity}`));
+totalQuantity.innerHTML = calculateTotalQuantity.reduce(reducer,0);
+//console.log(calculateTotalQuantity);
+
+//------------------------Total price-------------------//
+
+calculateTotalPrice.push( parseInt(`${element.quantity}`) * parseInt(`${elements.price}`));
+totalPrice.innerHTML = calculateTotalPrice.reduce(reducer,0);  
+//console.log(calculateTotalPrice);
+
+//----------------------Delete a product from the cart and from the local storage------------------//
+
+function deleteProduct(){
+const deleteItem = document.querySelectorAll(".deleteItem");
+
+// For each loop for the querySelectorAll, every delete button listens to an event
+deleteItem.forEach(function (del) {
+  del.addEventListener("click", function() {  
+
+    let idDelete = this.closest("[data-id]").dataset.id ; 
+    let colorDelete = this.closest("[data-color]").dataset.color;
+    let indexDelete = productsInLocalStorage.findIndex( x => x.id == idDelete && x.color == colorDelete); 
+    productsInLocalStorage.splice(indexDelete,1); 
+    localStorage.setItem("item", JSON.stringify(productsInLocalStorage));
+    alert("L'article a été supprimé")
+    location.reload();
+  }
+)
+})
+}
+//------------------------Modify quantity-----------------------------------------//
+
+function ModifyQuantity() {
+  let changeQuantity = document.querySelectorAll(".itemQuantity");
   
-let productInfo = {
-  nameOfProduct: info.name,
-  idOfProduct: info._id,
-  colorOfProduct: info.colors,
-  priceOfProduct: info.price,
-  quantityOfProduct: item.quantity 
+  changeQuantity.forEach(function (quantityChange) {
+    quantityChange.addEventListener("change" , function() {
+
+      let idChange = this.closest("[data-id]").dataset.id ; 
+      let colorChange = this.closest("[data-color]").dataset.color;
+      let newQuantity = parseInt(this.value);
+      let indexChange = productsInLocalStorage.findIndex( x => x.id == idChange && x.color == colorChange);
+      /* console.log(newQuantity);
+      console.log(indexChange);
+      console.log(idChange);
+      console.log(colorChange); */
+      productsInLocalStorage[indexChange].quantity = newQuantity;
+      localStorage.setItem("item", JSON.stringify(productsInLocalStorage));
+      alert("La quantité a été modifiée")
+      location.reload();
+    });
+  });
 }
-    //console.log(productInfo); 
-   
-    console.log(info);
-    //console.log(item);
-    //console.log(productId);
-    function totals(){
-      
-      let sumQuantity = item.quantity;
-      let sumPrice = (productInfo.quantityOfProduct * productInfo.priceOfProduct);
-    
-      info._id == productId && productInfo.quantityOfProduct == productQuantity;
-      info._id == productId && productInfo.priceOfProduct == totalPrice; 
-      
-    
-      totalQuantity.innerHTML = sumQuantity;
-      totalPrice.innerHTML = sumPrice;
-    
-      console.log(sumQuantity);
-      console.log(sumPrice);
-      console.log(totalQuantity);
-    
-    }
-    totals();
-        
-    } 
+})
+.catch((error) => {
+console.error('Error', error);
+
+    });
+  });
 }
 
-)} 
-}};cartItems()
 
+//-------------------------------------Order form---------------------------------------//
 
+// DOM element for the form
 
+const form = document.querySelector(".cart__order__form");
 
-
-
-
-
-
+// RegExp
+let namesRegExp = new RegExp("^[a-zA-Zàâäéèêëïîôöùûüç' -]{2,}$");
+let addressRegExp = new RegExp("^[0-9]{1,4}[a-zA-Z0-9àâäéèêëïîôöùûüç '.,-]{3,}$");
+let cityRegExp = new RegExp("^[a-zA-Z0-9àâäéèêëïîôöùûüç' -]{3,60}$");
+let emailRegExp = new RegExp("^[a-zA-Z0-9àâäéèêëïîôöùûüç.-_]+[@]{1}[a-zA-Z0-9.-_]+[.][a-z]{2,10}$");
 
 
 
